9be7f176e80b0f8d43f9acdccbe36b33499c3731
Ivan Maddaluno <ivanmaddaluno1993@gmail.com>
2021-04-25 03:30:24 +0200

    images deleted

--- src/pages/Works/Works.pages.jsx
+++ src/pages/Works/Works.pages.jsx
@@ -1,15 +1,11 @@
 import React, { Suspense } from "react";
 import "./Works.styles.scss";
-import work1 from "../../assets/images/work1.jpg";
 import showCase_1 from "../../assets/images/showCase_1.png";
-import comingSoon2 from "../../assets/images/comingSoon2.jpg";
-import MyBtn from "../../components/UI/MyBtn//MyBtn.ui.component.jsx";
+
 import react from "../../assets/images/react.png";
 import sass from "../../assets/images/sass.png";
 import analytics from "../../assets/images/analytics.png";
-import { TimelineMax, Expo } from "gsap";
 
-import Work from "../../components/Project/Project.component.jsx";
 import Project from "../../components/Project/Project.component.jsx";
 import LoaderBox from "../Homepage/LoaderBox.component.jsx";
 import { applyClassOnHover } from "../../util/fn";
